06c39137980865f2245d0da65baf5bdc279c6d7a
tmalesa021 <tmalesa021@student.wethinkcode.co.za>
2023-06-07 03:41:49 +0200

    Automated-Withdrawal

---
+++ src/main/java/com/momentum/automatedwithdrawal/AutomatedWithdrawalApplication.java
@@ -0,0 +1,13 @@
+package com.momentum.automatedwithdrawal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedWithdrawalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatedWithdrawalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/momentum/automatedwithdrawal/AutomatedWithdrawalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momentum.automatedwithdrawal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedWithdrawalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
